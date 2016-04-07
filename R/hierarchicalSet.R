78949e336e0a444f689a56a93a9d1c4ef7e50ec4
Thomas Dybdal Pedersen <thomasp85@gmail.com>
2016-04-07 14:07:22 +0200

    Add getters for HierarchicalSet class

--- R/hierarchicalSet.R
+++ R/hierarchicalSet.R
@@ -1,53 +1,181 @@
 #' @include hSet.R
 NULL
 
-#' @describeIn hSet Print method for hierarchicalSet objects
+#' @describeIn create_hierarchy Print method for hierarchicalSet objects
 #'
 #' @param x A hierarchicalSet object
 #'
-#' @export
-#'
-    cat('A hierarchicalSet object\n\n')
-    cat('                 Universe size: ', nrow(x$sets), '\n', sep = '')
-    cat('                Number of sets: ', length(x), '\n', sep = '')
-    cat('Number of independent clusters: ', length(x$clusters), '\n', sep = '')
-}
-#' @describeIn hSet Number of sets in a hierarchicalSet object
+#' @param ... Currently ignored
 #'
 #' @export
 #'
-length.hierarchicalSet <- function(x) {
-    ncol(x$sets)
 print.HierarchicalSet <- function(x, ...) {
+    cat('A HierarchicalSet object\n\n')
+    cat('                 Universe size: ', n_elements(x), '\n', sep = '')
+    cat('                Number of sets: ', n_sets(x), '\n', sep = '')
+    cat('Number of independent clusters: ', n_clusters(x), '\n', sep = '')
 }
-#' @describeIn hSet Extract dendrogram objects from hierarchicalSet objects
+#' @describeIn create_hierarchy Extract dendrogram objects from hierarchicalSet objects
 #'
 #' @param i The index of the dendrogram
 #'
 #' @export
 #'
-    x$clusters[[i]]
 `[[.HierarchicalSet` <- function(x, i) {
+    clusters(x)[[i]]
 }
-#' @describeIn hSet Subset hierarchicalSet object by dendrogram (preserving set
+#' @describeIn create_hierarchy Subset hierarchicalSet object by dendrogram (preserving set
 #' information and class)
 #'
 #' @importFrom stats dendrapply
 #'
 #' @export
 #'
-    clusters <- x$clusters[i]
-    includedSets <- sort(unlist(lapply(clusters, attr, 'memberSets')))
-    sets <- x$sets[, includedSets, drop = FALSE]
-    sets <- sets[rowSums(sets) != 0, , drop = FALSE]
-    clusters <- lapply(clusters, function(den) {
 `[.HierarchicalSet` <- function(x, i) {
+    newClusters <- clusters(x)[i]
+    includedSets <- sort(unlist(lapply(newClusters, attr, 'memberSets')))
+    newSets <- sets(x)[, includedSets, drop = FALSE]
+    newSets <- newSets[rowSums(newSets) != 0, , drop = FALSE]
+    newClusters <- lapply(newClusters, function(den) {
         dendrapply(den, function(node) {
             attr(node, 'memberSets') <- match(attr(node, 'memberSets'), includedSets)
             node
         })
     })
-    res <- list(sets = sets, clusters = clusters)
+    res <- list(sets = newSets, clusters = newClusters)
     class(res) <- 'hierarchicalSet'
     res
 }
+#' Getters for HierarchicalSet objects
+#'
+#' These utility functions makes it easy to extract raw information from a
+#' HierarchicalSet object.
+#'
+#' @param x A HierarchicalSet object
+#'
+#' @return depending on the function. See details
+#'
+#' @details
+#' \code{sets} Returns a ngCMatrix with sets as columns and elements as rows.
+#'
+#' @export
+#'
+#' @rdname hs-get
+#'
+#' @name HierarchicalSet-getters
+#'
+#' @examples
+#' data('twitter')
+#'
+#' twitSet <- create_hierarchy(twitter)
+#'
+#' # Get the sets as a presence/absence matrix
+#' head(sets(twitSet))
+#'
+sets <- function(x) UseMethod('sets')
+#' @describeIn create_hierarchy Extract the sets as a sparse matrix
+#'
+#' @export
+sets.HierarchicalSet <- function(x) x$sets
+#' @rdname hs-get
+#'
+#' @details
+#' \code{clusters} returns a list of dendrograms with the clustering in the
+#' HierarchicalSet object
+#'
+#' @export
+#'
+#' @examples
+#' # Get the clustering of the HierarchicalSet object
+#' clusters(twitSet)
+#'
+clusters <- function(x) UseMethod('clusters')
+#' @describeIn create_hierarchy Extract the clusters as a list of dendrograms
+#'
+#' @export
+clusters.HierarchicalSet <- function(x) x$clusters
+#' @rdname hs-get
+#'
+#' @details
+#' \code{set_names} returns a character vector with the names of the sets.
+#'
+#' @export
+#'
+#' @examples
+#' # Get the set names
+#' set_names(twitSet)
+#'
+set_names <- function(x) UseMethod('set_names')
+#' @describeIn create_hierarchy Get the names of the sets
+#'
+#' @export
+set_names.HierarchicalSet <- function(x) colnames(sets(x))
+#' @rdname hs-get
+#'
+#' @details
+#' \code{element_names} returns a character vector with the names of the
+#' elements
+#'
+#' @export
+#'
+#' @examples
+#' # Get the element names or NULL if they are unnamed
+#' element_names(twitSet)
+#'
+element_names <- function(x) UseMethod('element_names')
+#' @describeIn create_hierarchy Get the names of the elements
+#'
+#' @export
+element_names.HierarchicalSet <- function(x) rownames(sets(x))
+#' @rdname hs-get
+#'
+#' @details
+#' \code{n_sets} returns the number of sets
+#'
+#' @export
+#'
+#' @examples
+#' # Get the number of sets
+#' n_sets(twitSet)
+#'
+n_sets <- function(x) UseMethod('n_sets')
+#' @describeIn create_hierarchy Get the number of sets
+#'
+#' @export
+n_sets.HierarchicalSet <- function(x) ncol(x$sets)
+#' @describeIn create_hierarchy Get the number of sets
+#'
+#' @export
+length.HierarchicalSet <- n_sets.HierarchicalSet
+#' @rdname hs-get
+#'
+#' @details
+#' \code{n_elements} returns the number of elements
+#'
+#' @export
+#'
+#' @examples
+#' # Get the number of elements
+#' n_elements(twitSet)
+#'
+n_elements <- function(x) UseMethod('n_elements')
+#' @describeIn create_hierarchy Get the number of elements
+#'
+#' @export
+n_elements.HierarchicalSet <- function(x) nrow(x$sets)
+#' @rdname hs-get
+#'
+#' @details
+#' \code{n_clusters} returns the number of independent set families
+#'
+#' @export
+#'
+#' @examples
+#' # Get the number of independent clusters
+#' n_clusters(twitSet)
+#'
+n_clusters <- function(x) UseMethod('n_clusters')
+#' @describeIn create_hierarchy Get the number of clusters
+#'
+#' @export
+n_clusters.HierarchicalSet <- function(x) length(clusters(x))
