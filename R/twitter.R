27c1b3e16db5d53b8a582a68880d6777d1012ed6
Thomas Dybdal Pedersen <thomasp85@gmail.com>
2016-04-07 13:59:56 +0200

    Add twitter dataset

---
+++ R/twitter.R
@@ -0,0 +1,12 @@
+#' Followers of 100 twitter users
+#'
+#' This dataset captures the followers of 100 highly followed twitter users. The
+#' dataset is anonymized and based on the ego-Twitter network from Stanford
+#' Large Network Dataset Collection.
+#'
+#' @format
+#' A ngCMatrix with the sets (users) as columns and elements (followers) as rows
+#'
+#' @source \url{https://snap.stanford.edu/data/}
+#'
+'twitter'
